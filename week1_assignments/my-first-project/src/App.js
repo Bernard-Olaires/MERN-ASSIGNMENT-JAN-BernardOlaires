2494fbb0b6950f6f531a0602ddd5c66331a6a93a
Bernard Olaires <bolairesjr@gmail.com>
2023-01-15 16:42:38 -0800

    Coding Dojo First JXS assignment

---
+++ week1_assignments/my-first-project/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <p>Things i Need to do:</p>
+      <ul>
+        <li>Learn React</li>
+        <li>Save More Money</li>
+        <li>Make Better Decisions</li>
+        <li>Have fun</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
