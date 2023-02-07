24db9787ac27f110c1efd04f558e56edf088bbe2
Bernard Olaires <bolairesjr@gmail.com>
2023-02-06 18:30:39 -0800

    Faker update

---
+++ week4_assignments/coin_flipping.js
@@ -0,0 +1,29 @@
+function tossCoin() {
+        return Math.random() > 0.5 ? "heads" : "tails";
+    }
+
+function fiveHeadsSync() {
+        let headsCount = 0;
+        let attempts = 0;
+        while(headsCount < 5) {
+            attempts++;
+            let result = tossCoin();
+            console.log(`${result} was flipped`);
+            if(result === "heads") {
+                headsCount++;
+            } else {
+                headsCount = 0;
+            }
+        }
+        return `It took ${attempts} tries to flip five "heads"`;
+    }
+    console.log( fiveHeadsSync() );
+    console.log( "This is run after the fiveHeadsSync function completes" );
+
+const fiveARow = new Promise((resolve, reject) =>{
+    let headsCount = 0;
+    let attempts = 0;
+    if(headsCount < 5) {
+        resolve("")
+    }
+});
--- week4_assignments/faker_api/server.js
+++ week4_assignments/faker_api/server.js
@@ -1,8 +1,40 @@
+const faker = require("faker");
+console.log(faker.datatype.uuid());
 const express = require("express");
 const app = express();
 const port = 8000;
 
+// creating a faker variable
+
+const userObject = () => ({
+    id: faker.datatype.uuid(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    phoneNumber: faker.phone.phoneNumber(),
+    email: faker.interet.email(),
+    password: faker.inter.password(),
+
+
+})
+
+const gerateCompayObject = () => ({
+    _id: faker.datatype.uuid(),
+    name: faker.address.cityName(),
+    zipCode: faker.address.zipCode(),
+    country: faker.address.country(),
+})
 
 // make sure these lines are above any app.get or app.post code blocks
-app.use( express.json() );
-app.use( express.urlencoded({ extended: true }) );
+app.use( express.json());
+app.use( express.urlencoded({ extended: true }));
+
+
+app.get('/api/user/new', (request, response) =>{
+    const newUser = userObject()
+    request.json(newUser)
+})
+
+
+// this needs to be below the other code blocks 
+// we have to tell express what port to listen at
+app.listen( port, () => console.log(`Listening on port: ${port}`) );
